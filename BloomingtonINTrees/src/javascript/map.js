1718b1579d2d6b14301afd26eaec3dad6f719467
R Wolfe <rochelle.wolfe@davey.com>
2019-10-29 12:08:55 -0400

    removed consolelogs

--- BloomingtonINTrees/src/javascript/map.js
+++ BloomingtonINTrees/src/javascript/map.js
@@ -59,7 +59,6 @@ $(document).ready(function () {
         RasterLayer,                    //23
         RasterFunction) {                 //24
 
-        console.log("inside the require in map.js");    
         dojo.require("esri.tasks.query"); //part of 1st attempt setGeoPopups https://developers.arcgis.com/javascript/3/jssamples/query_clickinfowindow.html
 
         var layersURL = "https://gis.davey.com/arcgis/rest/services/BloomingtonIN/BloomintonIN/MapServer";
@@ -113,7 +112,7 @@ $(document).ready(function () {
 
 
         makeTheLegend = function (mapDetailsFromJson, currentMap) {
-            console.log("makeTheLegend for: "+ mapDetailsFromJson.containerID);
+            // console.log("makeTheLegend for: "+ mapDetailsFromJson.containerID);
             var detailLayer = mapLayers[mapDetailsFromJson.searchLayer].layerID;
             var imageParameters = new ImageParameters();
             imageParameters.layerIds = detailLayer;
@@ -135,7 +134,7 @@ $(document).ready(function () {
 
         // mapDetails is the list of individula map details from all of the Slides
         $.each(mapDetails, function (k, slideMap) {
-            console.log(slideMap.containerID);
+            // console.log(slideMap.containerID);
             var currentMap = new Map(slideMap.containerID, {
                 basemap: slideMap.basemap,
                 center: slideMap.mapCenter,
@@ -173,7 +172,7 @@ $(document).ready(function () {
                 
                 var slide = mapLayers[layerNumber]; //this individual layer of the layers that will be on this map
                 var newLayer;
-                console.log("layerNumber: "+layerNumber);
+                // console.log("layerNumber: "+layerNumber);
 
                 newLayer = new ArcGISDynamicMapServiceLayer("https://gis.davey.com/arcgis/rest/services/BloomingtonIN/BloomintonIN/MapServer");
                 newLayer.setVisibleLayers([slide.layerID]);
@@ -205,16 +204,12 @@ $(document).ready(function () {
             currentMap.on("click", function (event) {
                 setPopups(event, slideMap, currentMap);
             }); //end on-click
-            console.log("at the end of for-each-map");
         }); //end .each
-        console.log("after for-each-map");
 
 
     }); //end require/function
-    console.log("end of doc.ready");
 
 }); //end doc.ready
-console.log("after doc.ready");
 
 
 // var tryThisFunction = function(map){
